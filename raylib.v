54f1f191e84c461897a92d0be2a80002a26906ac
Justin Hurstwright <jryanhurst@gmail.com>
2019-10-20 12:55:21 -0700

    Finish core values

--- raylib.v
+++ raylib.v
@@ -839,6 +839,86 @@ pub fn get_gamepad_axis_movement(gamepad, axis int) f32 {
 	return C.GetGamepadAxisMovement(gamepad, axis)
 }
 
+//------------------------------------------------------------------------------------
+// Gestures and Touch Handling Functions (Module: gestures)
+//------------------------------------------------------------------------------------
+
+// Enable a set of gestures using flags
+pub fn set_gestures_enabled(gestureFlags u32) {
+	C.SetGesturesEnabled(gestureFlags)
+}
+
+// Check if a gesture have been detected
+pub fn is_gesture_detected(gesture int) bool {
+	return C.IsGestureDetected(gesture)
+}
+
+// Get latest detected gesture
+pub fn get_gesture_detected() int {
+	return C.GetGestureDetected()
+}
+
+// Get touch points count
+pub fn get_touch_points_count() int {
+	return C.GetTouchPointsCount()
+}
+
+// Get gesture hold time in milliseconds
+pub fn get_gesture_hold_duration() f32 {
+	return C.GetGestureHoldDuration()
+}
+// Get gesture drag vector
+pub fn get_gesture_drag_vector() Vector2 {
+	return C.GetGestureDragVector()
+}
+
+// Get gesture drag angle
+pub fn get_desture_drag_angle() f32 {
+	return C.GetGestureDragAngle()
+}
+
+// Get gesture pinch delta
+pub fn get_gesture_pinch_vector() Vector2 {
+	return C.GetGesturePinchVector()
+}
+
+// Get gesture pinch angle
+pub fn get_gesture_pinch_angle() f32 {
+	return C.GetGesturePinchAngle()
+}
+
+//------------------------------------------------------------------------------------
+// Camera System Functions (Module: camera)
+//------------------------------------------------------------------------------------
+// Set camera mode (multiple camera modes available)
+pub fn set_camera_mode(camera Camera3D, mode int) {
+	C.SetCameraMode(camera, mode)
+}
+
+// Update camera position for selected mode
+pub fn update_camera(camera &Camera3D) {
+	C.UpdateCamera(camera)
+}
+
+// Set camera pan key to combine with mouse movement (free camera)
+pub fn set_camera_pan_control(panKey int) {
+	C.SetCameraPanControl(panKey)
+}
+
+// Set camera alt key to combine with mouse movement (free camera)
+pub fn set_camera_alt_control(altKey int) {
+	C.SetCameraAltControl(altKey)
+}
+
+// Set camera smooth zoom key to combine with mouse (free camera)
+pub fn set_camera_smooth_zoom_control(szKey int) {
+	C.SetCameraSmoothZoomControl(szKey)
+}
+
+// Set camera move controls (1st person and 3rd person cameras)
+pub fn set_camera_move_controls(frontKey, backKey, rightKey, leftKey, upKey, downKey int) {
+	C.SetCameraMoveControls(frontKey, backKey, rightKey, leftKey, upKey, downKey)
+}
 
 
 // Shape Drawing Functions
