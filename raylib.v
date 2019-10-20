862231fd57fcdfa04cab5e3f2954c8fb8d0c839e
Justin Hurstwright <jryanhurst@gmail.com>
2019-10-20 12:28:07 -0700

    add in Gamepad controls

--- raylib.v
+++ raylib.v
@@ -599,6 +599,60 @@ pub fn set_exit_key(key int) {
 	C.SetExitKey(key)
 }
 
+
+// Input-related functions: gamepads
+//
+
+// Detect if a gamepad is available
+pub fn is_gamepad_available(gamepad int) bool {
+	return C.IsGamepadAvailable(gamepad)
+}
+
+// Check gamepad name (if available)
+pub fn is_gamepad_name(gamepad int, name string) bool {
+	return C.IsGamepadName(gamepad, name.str)
+}
+
+// Return gamepad internal name id
+pub fn get_gamepad_name(gamepad int) string {
+	return string(byteptr(C.GetGamepadName(gamepad)))
+}
+
+// Detect if a gamepad button has been pressed once
+pub fn is_gamepad_button_pressed(gamepad, button int) bool {
+	return C.IsGamepadButtonPressed(gamepad, button)
+}
+
+// Detect if a gamepad button is being pressed
+pub fn is_gamepad_button_down(gamepad, button int) bool {
+	return C.IsGamepadButtonDown(gamepad, button)
+}
+
+// Detect if a gamepad button has been released once
+pub fn is_gamepad_button_releaed(gamepad, button int) bool {
+	return C.IsGamepadButtonReleased(gamepad, button)
+}
+
+// Detect if a gamepad button is NOT being pressed
+pub fn is_gamepad_button_up(gamepad, button int) bool {
+	return C.IsGamepadButtonUp(gamepad, button)
+}
+
+// Get the last gamepad button pressed
+pub fn get_gamepad_button_pressed() int {
+	return C.GetGamepadButtonPressed()
+}
+
+// Return gamepad axis count for a gamepad
+pub fn get_gamepad_axis_count(gamepad int) int {
+	return C.GetGamepadAxisCount(gamepad)
+}
+
+// Return axis movement value for a gamepad axis
+pub fn get_gamepad_axis_movement(gamepad, axis int) f32 {
+	return C.GetGamepadAxisMovement(gamepad, axis)
+}
+
 // Color Related Functions
 pub fn color_to_int(color Color) int {
 	return C.ColorToInt(color)
