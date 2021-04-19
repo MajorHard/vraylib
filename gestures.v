bd0892aca67c7b32f4e843f7d9ef5659d97c33cd
wolfi <wolfi@disroot.org>
2021-04-19 19:03:13 +0200

    add gesture constans

--- gestures.v
+++ gestures.v
@@ -1,5 +1,19 @@
 module vraylib
 
+const(
+	gesture_none        = 0
+	gesture_tap         = 1
+	gesture_doubletap   = 2
+	gesture_hold        = 4
+	gesture_drag        = 8
+	gesture_swipe_right = 16
+	gesture_swipe_left  = 32
+	gesture_swipe_up    = 64
+	gesture_swipe_down  = 128
+	gesture_pinch_in    = 256
+	gesture_pinch_out   = 512
+)
+
 //------------------------------------------------------------------------------------
 // Gestures and Touch Handling Functions (Module: gestures)
 //------------------------------------------------------------------------------------
