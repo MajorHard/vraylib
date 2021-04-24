9cff26b7f1b050097b8a52a5e88526c65cf3c6fa
wolfi <wolfi@disroot.org>
2021-04-24 13:34:00 +0200

    fix return values

--- physac.v
+++ physac.v
@@ -12,18 +12,18 @@ pub fn vector2_add(v1 C.Vector2, v2 C.Vector2) C.Vector2 {
 }
 
 
-fn C.Vector2Length(v C.Vector2) C.Vector2
-pub fn vector2_length(v C.Vector2) C.Vector2 {
+fn C.Vector2Length(v C.Vector2) f32
+pub fn vector2_length(v C.Vector2) f32 {
 	return C.Vector2Length(v)
 }
 
-fn C.Vector2LengthSqr(v C.Vector2) C.Vector2
-pub fn vector2_length_sqr(v C.Vector2) C.Vector2 {
+fn C.Vector2LengthSqr(v C.Vector2) f32
+pub fn vector2_length_sqr(v C.Vector2) f32 {
 	return C.Vector2LengthSqr(v)
 }
 
-fn C.Vector2DotProduct(v1 C.Vector2, v2 C.Vector2) C.Vector2
-pub fn vector2_dot_product(v1 C.Vector2, v2 C.Vector2) C.Vector2 {
+fn C.Vector2DotProduct(v1 C.Vector2, v2 C.Vector2) f32
+pub fn vector2_dot_product(v1 C.Vector2, v2 C.Vector2) f32 {
 	return C.Vector2DotProduct(v1, v2)
 }
 
@@ -32,8 +32,8 @@ pub fn vector2_distrance(v1 C.Vector2, v2 C.Vector2) C.Vector2 {
 	return C.Vector2Distance(v1, v2)
 }
 
-fn C.Vector2Angle(v1 C.Vector2, v2 C.Vector2) C.Vector2
-pub fn vector2_angle(v1 C.Vector2, v2 C.Vector2) C.Vector2 {
+fn C.Vector2Angle(v1 C.Vector2, v2 C.Vector2) f32
+pub fn vector2_angle(v1 C.Vector2, v2 C.Vector2) f32 {
 	return C.Vector2Angle(v1, v2)
 }
 
